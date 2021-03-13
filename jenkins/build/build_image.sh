817703934d4fc8d69d29a17b65cb2f9830855b87
jenkins <jenkins@localhost.localdomain>
2021-03-13 08:48:34 -0500

    fixed dockerfile path

--- jenkins/build/build_image.sh
+++ jenkins/build/build_image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Copy the new jar to the build location
-cp -f my-app/target/*.jar . 
+#Copy the new jar to the build location
+cp -f my-app/target/my-app-*.jar . 
 echo "current working dir: $(pwd)"
 echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 
-cd ../.. && docker build -t dorivin1/maven-hello-world:$BUILD_TAG
+docker build -t dorivin1/maven-hello-world:$BUILD_TAG
