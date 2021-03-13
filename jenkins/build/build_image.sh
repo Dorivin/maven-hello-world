b8e191f6b30603802506ba278ac70e77e4de2be7
jenkins <jenkins@localhost.localdomain>
2021-03-13 08:20:47 -0500

    fixed path to myapp

--- jenkins/build/build_image.sh
+++ jenkins/build/build_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy the new jar to the build location
-cp -f ../../my-app/target/*.jar . 
+cp -f my-app/target/*.jar . 
 
 echo "****************************"
 echo "** Building Docker Image ***"
