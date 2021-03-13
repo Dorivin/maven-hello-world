d5cfcd1dac5337ebdee25976f53fd5f26ee233a9
jenkins <jenkins@localhost.localdomain>
2021-03-13 06:28:12 -0500

    added sock access

--- jenkins/build/build_jar.sh
+++ jenkins/build/build_jar.sh
@@ -6,4 +6,4 @@ echo "***************************"
 
 JAVA_PROJECT_DIR=/home/jenkins/Refael_Exercise/maven-hello-world/my-app
 
-docker run --rm  -v  $JAVA_PROJECT_DIR:/app -w /app maven:3-alpine "$@"
+docker run --rm  -v $JAVA_PROJECT_DIR:/app -v var/run/docker.sock:var/run/docker.sock -w /app maven:3-alpine "$@"
