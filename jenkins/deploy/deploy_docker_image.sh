5d41f27f2ae153085552de934897edd3be79b1d4
jenkins <jenkins@localhost.localdomain>
2021-03-13 09:19:47 -0500

    added BUILD_NUMBER fix

--- jenkins/deploy/deploy_docker_image.sh
+++ jenkins/deploy/deploy_docker_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run dorivin1/maven-hello-world:latest
+docker run dorivin1/maven-hello-world:$BUILD_NUMBER
--- jenkins/push/push_docker_image.sh
+++ jenkins/push/push_docker_image.sh
@@ -9,6 +9,6 @@ IMAGE="maven-hello-world"
 echo "** Logging in ***"
 docker login -u dorivin1 -p $DOCKERHUB_PASS
 echo "*** Tagging image ***"
-docker tag $IMAGE:$BUILD_TAG dorivin1/$IMAGE:$BUILD_TAG
+docker tag $IMAGE:$BUILD_NUMBER dorivin1/$IMAGE:$BUILD_NUMBER
 echo "*** Pushing image ***"
-docker push dorivin1/$IMAGE:$BUILD_TAG
+docker push dorivin1/$IMAGE:$BUILD_NUMBER
