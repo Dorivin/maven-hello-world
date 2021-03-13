8f79839575836516261c58871c6e87b9de2af6be
jenkins <jenkins@localhost.localdomain>
2021-03-13 08:26:29 -0500

    pwd

--- jenkins/build/build_image.sh
+++ jenkins/build/build_image.sh
@@ -2,7 +2,7 @@
 
 # Copy the new jar to the build location
 cp -f my-app/target/*.jar . 
-
+echo "current working dir: $(pwd)"
 echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
