5c62a4ab30fa3e24229779ae56edd337be1624c2
jenkins <jenkins@localhost.localdomain>
2021-03-14 12:12:47 -0400

    fixed build_jar.sh

--- jenkins/build/build_jar.sh
+++ jenkins/build/build_jar.sh
@@ -3,6 +3,6 @@
 echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
-WORKSPACE_DIR=/var/jenkins_home/workspace/Maven-CI-CD/my-app
+WORKSPACE_DIR=/home/ec2-user/jenkins-data/jenkins_home/workspace/Maven-CI-CD/my-app
 
-docker run --rm -v $WORKSPACE_DIR:$WORKSPACE_DIR -w $WORKSPACE_DIR maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE_DIR:/app -w /app maven:3-alpine "$@"
