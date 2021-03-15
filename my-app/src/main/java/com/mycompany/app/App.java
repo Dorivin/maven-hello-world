cb32172a2313592ee26e521b6d30e080f3809a9d
EC2 Default User <ec2-user@ip-172-31-35-142.us-east-2.compute.internal>
2021-03-15 14:33:02 +0000

    another check

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Greetings, Dor Wein!" );
+        System.out.println( "Greetings, Dor Wein! #2" );
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
